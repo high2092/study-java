92aa149f66201b45ec696e4dce76a3f8a56f3e97
hhyuw <high2092@gmail.com>
2023-02-12 01:23:10 +0900

    서로 다른 난수 3개 생성

--- baseball/src/Main.java
+++ baseball/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.*;
+
 public class Main {
+    private static final int STRIKE_CRITERIA = 3;
+
+    private static final List<Integer> DIGITS = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
+
+    private static int[] generateHitNumbers() {
+        int[] hitNumbers = new int[STRIKE_CRITERIA];
+        return Arrays.stream(hitNumbers).map(hitNumber -> {
+            int idx = (int)(Math.random() * DIGITS.size());
+            return DIGITS.remove(idx);
+        }).toArray();
+
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Arrays.stream(generateHitNumbers()).forEach(System.out::println);
     }
 }
