efa0f8b188ed215e74592569a5e71b81ad480839
hhyuw <high2092@gmail.com>
2023-02-12 02:33:30 +0900

    메소드당 10라인 넘어가지 않도록 메소드 분리

--- baseball/src/Main.java
+++ baseball/src/Main.java
@@ -18,6 +18,8 @@ public class Main {
     private static final int MISS = 0;
     private static final int STRIKE = 1;
     private static final int BALL = 2;
+    private static final Scanner scanner = new Scanner(System.in);
+    private static boolean isGameOver = false;
 
     private static int check(int idx, int guess) {
         for (int i = 0; i < STRIKE_CRITERIA; i++) { // for문을 forEach로 대체하는 것의 장점이 떠오르지 않는 경우도 종종 맞닥뜨림
@@ -26,14 +28,12 @@ public class Main {
         return MISS;
     }
 
-    private static void printGuessResult() {
+    private static void printGuessResult(int[] guessResultCount) {
         System.out.println("스트라이크: " + guessResultCount[STRIKE]);
         System.out.println("볼: " + guessResultCount[BALL]);
     }
 
-    public static void main(String[] args) {
-        Arrays.stream(hitNumbers).forEach(System.out::println);
-        Scanner scanner = new Scanner(System.in);
+    private static void register() {
         System.out.println("당신의 이름은?");
         String username = scanner.nextLine();
         System.out.println("당신의 나이는?");
@@ -41,23 +41,26 @@ public class Main {
         System.out.println("등록 완료: " + username + " (" + age + ")");
 
         System.out.println("게임을 시작합니다.");
+    }
 
-        boolean isGameOver = false;
-        while (true) {
-            System.out.println("3개의 숫자는 순서대로 무엇일까요? (0 이상 9 이하의 서로 다른 정수, 공백으로 구분)");
-            guessResultCount = new int[STRIKE_CRITERIA];
+    private static int[] playTurn() {
+        System.out.println("3개의 숫자는 순서대로 무엇일까요? (0 이상 9 이하의 서로 다른 정수, 공백으로 구분)");
+        guessResultCount = new int[STRIKE_CRITERIA];
 
-            for (int i = 0; i < STRIKE_CRITERIA; i++) {
-                int guess = scanner.nextInt(); // TODO: n개의 입력을 모두 받고 유효성 검증을 마친 후 연산 시작하기
-                int result = check(i, guess);
-                guessResultCount[result]++;
-            }
+        for (int i = 0; i < STRIKE_CRITERIA; i++) {
+            int guess = scanner.nextInt(); // TODO: n개의 입력을 모두 받고 유효성 검증을 마친 후 연산 시작하기
+            int result = check(i, guess);
+            guessResultCount[result]++;
+        }
+        return guessResultCount;
+    }
 
-            printGuessResult();
-
+    public static void main(String[] args) {
+        register();
+        while (isGameOver == false) {
+            int[] guessResult = playTurn();
+            printGuessResult(guessResult);
             isGameOver = guessResultCount[STRIKE] == STRIKE_CRITERIA;
-
-            if (isGameOver) break;
         }
     }
 }
