60ee4690b892309a22040a876d9cf888bd6f07a9
hhyuw <high2092@gmail.com>
2023-03-06 21:44:10 +0900

    URL Parameter

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -11,5 +12,11 @@ public class HelloController {
     public  String hello(Model model) {
         model.addAttribute("data", "hhyuw");
         return "hello"; // 템플릿 파일 이름
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) { // @RequestParam 의 매개변수에는 URL Parameter 이름을 적는다.
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
