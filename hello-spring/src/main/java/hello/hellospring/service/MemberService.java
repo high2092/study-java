99c758fbfeb0d3790ba031b88071f0af4a92d8ba
hhyuw <high2092@gmail.com>
2023-03-07 16:13:15 +0900

    Service

---
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,34 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("회원명 중복");
+                });
+    }
+
+    public List<Member> ViewAllMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
---
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,60 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+    MemberService memberService = new MemberService();
+    MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach() {
+        memberRepository.clear();
+    }
+
+    @Test
+    void join() {
+        // given
+        Member member = new Member();
+        member.setName("ha");
+
+        // when
+        Long id = memberService.join(member);
+
+        // then
+        Member result = memberService.findMember(id).get();
+        assertThat(result.getName()).isEqualTo(member.getName());
+    }
+
+    @Test
+    public void joinDuplicate() {
+        // given
+        Member member1 = new Member();
+        member1.setName("ha");
+
+        Member member2 = new Member();
+        member2.setName("ha");
+
+        // when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        // then
+        assertThat(e.getMessage()).isEqualTo("회원명 중복");
+    }
+
+    @Test
+    void viewAllMembers() {
+    }
+
+    @Test
+    void findMember() {
+    }
+}
