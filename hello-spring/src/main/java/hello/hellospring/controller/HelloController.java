58a6f4c28b3a40982abe1a13d11fbea31d450828
hhyuw <high2092@gmail.com>
2023-03-07 15:37:33 +0900

    API

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -18,5 +19,19 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model) { // @RequestParam 의 매개변수에는 URL Parameter 이름을 적는다.
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        return new Hello(name); // 객체 반환 시 JSON 문자열로 자동 변환
+    }
+
+    static class Hello {
+        private String name;
+
+        public Hello(String name) {
+            this.name = name;
+        }
     }
 }
