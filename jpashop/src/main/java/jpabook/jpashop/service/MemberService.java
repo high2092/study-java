b71ad79ea5142d22f6cc2f557b9cbb3926c9e6bc
hhyuw <high2092@gmail.com>
2023-03-24 19:17:35 +0900

    회원 서비스

---
+++ jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,45 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor // final 필드들 초기화
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+//    @Autowired
+//    public MemberService(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+
+    }
+
+    private void validateDuplicateMember(Member member) {
+        Member foundMember = memberRepository.findByName(member.getName());
+        if (foundMember != null) {
+            throw new IllegalStateException("회원명 중복");
+        }
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
