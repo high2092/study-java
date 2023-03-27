ef7ba3aea57f2a68394360262803680315e72239
hhyuw <high2092@gmail.com>
2023-03-27 22:43:51 +0900

    회원 수정 API

--- jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -4,11 +4,10 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,6 +26,14 @@ public class MemberApiController {
 
     }
 
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMember(@PathVariable Long id, @RequestBody @Valid UpdateMemberRequest request) {
+        memberService.update(id, request.getName());
+
+        Member member = memberService.findOne(id); // <- 수정 후 조회. 해야 하나?
+        return new UpdateMemberResponse(member.getId(), member.getName());
+    }
+
     @Getter
     static class CreateMemberRequest {
         @NotEmpty
@@ -40,5 +47,17 @@ public class MemberApiController {
         public CreateMemberResponse(Long id) {
             this.id = id;
         }
+    }
+
+    @Getter
+    static class UpdateMemberRequest {
+        private String name;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
     }
 }
